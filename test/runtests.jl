59facea72e41776e8a283004c9db7b4b238f3695
Michael Schlottke-Lakemper <michael@sloede.com>
2021-09-16 16:48:17 +0200

    Fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,16 +3,16 @@ using SmartShockFinder
 
 EXAMPLES_DIR = joinpath(pathof(SmartShockFinder) |> dirname |> dirname, "examples")
 
-@test_nowarn rm(joinpath("datasets"), recursive=true)
+@test_nowarn rm(joinpath("datasets"), recursive=true, force=true)
 
 @time @testset "SmartShockFinder" begin
   @test_nowarn mkpath(joinpath("datasets", "1d"))
 
   @testset "examples/1d/traindata_NNPPh.jl" begin
-    @test_nowarn include(joinpath("examples", "1d", "traindata_NNPPh.jl"))
+    @test_nowarn include(joinpath(EXAMPLES_DIR, "1d", "traindata_NNPPh.jl"))
 
     @test isfile(joinpath("datasets", "1d", "traindata1dNNPPh.h5"))
   end
 end
 
-@test_nowarn rm(joinpath("datasets"), recursive=true)
+@test_nowarn rm(joinpath("datasets"), recursive=true, force=true)
